f2eb325bfd55e9045679950bbbefc8fa63e904e1
Dasha Burgos <dasha.lewis@gmail.com>
2023-02-10 17:23:11 -0500

    problem three passed check

--- js/app.js
+++ js/app.js
@@ -54,11 +54,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let questionThreeSum = sum(a,b)[0];
+  let thirdTotalSum = sum(questionThreeSum, c)[0];
+  let thirdTotalSumString = `${a} and ${b} and ${c} sum to 16.`;
 
+  let questionThreeProduct = multiply(a,b)[0];
+  let thirdTotalProduct = multiply(questionThreeProduct, c)[0];
+  let thirdTotalProductString = `The product of ${a} and ${b} and ${c} is 140.`;
+
+  return [thirdTotalSum, thirdTotalProduct, thirdTotalSumString, thirdTotalProductString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
